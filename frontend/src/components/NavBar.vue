28f7bf0e14b5fe16c46d4ef47aa4521d16ef75a3
KeerthikaDuddupudi <keerthikadjnp@gmail>
2026-03-29 12:23:06 +0530

    corrected

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -125,7 +125,7 @@ export default {
       } else if (message.includes('cancel')) {
         icon = '❌'
         title = 'Appointment Cancelled'
-        link = '/appointments'
+        link = '/schedule'
         cta = 'View Cancellation'
       }
 
